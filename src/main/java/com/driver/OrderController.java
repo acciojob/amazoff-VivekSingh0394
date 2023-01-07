112469af00963cf5dc333abbc35fc68b338c0571
Ravi <rksnhes1999@gmail.com>
2023-01-07 17:52:07 +0530

    message

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -19,7 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("orders")
 public class OrderController {
 @Autowired
-OrderService orderService;
+OrderService orderService = new OrderService();
+//OrderService orderService;
+
 
     @PostMapping("/add-order")
     public ResponseEntity<String> addOrder(@RequestBody Order order){
--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -8,7 +8,8 @@ import java.util.List;
 @Service
 public class OrderService {
     @Autowired
-    OrderRepository orderRepository;
+    OrderRepository orderRepository = new OrderRepository();
+    //OrderRepository orderRepository;
     public void addOrder(Order order)
     {
         orderRepository.addOrderDb(order);
